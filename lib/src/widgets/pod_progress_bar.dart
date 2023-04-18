b81731da791bf06dbee1bf1987e61dfc97a0839c
Salih T <mhdsalih9656@gmail.com>
2023-04-18 16:55:54 +0530

    editted

--- lib/src/widgets/pod_progress_bar.dart
+++ lib/src/widgets/pod_progress_bar.dart
@@ -48,12 +48,7 @@ class _PodProgressBarState extends State<PodProgressBar> {
       final double relative = tapPos.dx / box.size.width;
       final Duration position =
           (videoPlayerValue?.duration ?? Duration.zero) * relative;
-      if (isPlayed == true) {
-        setState(() {
-          isPlayed = false;
-        });
-        _podCtr.videoCtr!.play();
-      }
+
       _podCtr.seekTo(position);
     }
   }
