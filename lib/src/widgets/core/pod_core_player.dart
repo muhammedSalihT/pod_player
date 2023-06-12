e00e48345027d016d5fb942fd3ad7bf23ad06bc2
muhammed salih t <mhdsalih9656@gmail.com>
2023-06-12 13:03:50 +0530

    editted

--- lib/src/widgets/core/pod_core_player.dart
+++ lib/src/widgets/core/pod_core_player.dart
@@ -28,7 +28,7 @@ class _PodCoreVideoPlayer extends StatelessWidget {
             tag: tag,
           ),
           child: Stack(
-            fit: StackFit.loose,
+            fit: StackFit.passthrough,
             children: [
               //remove a center wraper of aspect ratio here
               AspectRatio(
