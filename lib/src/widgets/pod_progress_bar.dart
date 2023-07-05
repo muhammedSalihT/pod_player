5db7d34b1a6a894780b73beced30eb0965784b25
muhammed salih t <mhdsalih9656@gmail.com>
2023-07-05 15:08:34 +0530

    commited

--- lib/src/widgets/pod_progress_bar.dart
+++ lib/src/widgets/pod_progress_bar.dart
@@ -257,3 +257,33 @@ class _ProgressBarPainter extends CustomPainter {
     );
   }
 }
+
+
+
+
+
+// Future<void> seekToRelativePosition(
+//     Offset globalPosition,
+//     BuildContext context,
+//   ) async {
+//     setState(() {
+//       isPlaying = false;
+//     });
+//     if (_podCtr.videoCtr!.value.isPlaying) {
+//       setState(() {
+//         isPlaying = true;
+//       });
+//       await _podCtr.videoCtr!.pause();
+//     }
+//     final box = context.findRenderObject() as RenderBox?;
+//     if (box != null) {
+//       final Offset tapPos = box.globalToLocal(globalPosition);
+//       final double relative = tapPos.dx / box.size.width;
+//       final Duration position =
+//           (videoPlayerValue?.duration ?? Duration.zero) * relative;
+//       await _podCtr.seekTo(position);
+//       if (isPlaying == true) {
+//         await _podCtr.videoCtr!.play();
+//       }
+//     }
+//   }
